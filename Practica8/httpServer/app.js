9b2c802687626c86d5ed0c88b099dc7603640085
Jesus Alcocer <jalcocer0@ucol.mx>
2023-02-24 08:28:15 -0600

    Practica 8

--- Practica8/httpServer/app.js
+++ Practica8/httpServer/app.js
@@ -1,6 +1,8 @@
+//Se instalaron las dependecias 'express'
 const express = require('express')
 const app = express()
 
+//Al correr en la terminal, se mostrara el 'Hello World' en el navegador, entonces, este estara esperando a la escucha de la terminal
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
